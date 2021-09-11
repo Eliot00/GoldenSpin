458127de4329b6beae6a4862b962d83550aec602
Elliot <hack00mind@gmail.com>
2021-09-11 18:00:14 +0800

    feat: short break

--- background.js
+++ background.js
@@ -1,9 +1,24 @@
-browser.alarms.onAlarm.addListener(() => {
+browser.alarms.onAlarm.addListener((alarmInfo) => {
+  let title;
+  let message;
+  switch (alarmInfo.name) {
+    case "cavalry-spin":
+      title = "Star Platinum! The World!";
+      message = "時停5分鐘，歐拉！";
+      break;
+    case "the-world":
+      title = "新循環";
+      message = "時間開始流動";
+      break;
+    default:
+      break;
+  }
+  
   browser.notifications.create("notification", {
     "type": "basic",
     "iconUrl": browser.runtime.getURL("icons/border-48.png"),
-    "title": "Star Platinum! The World!",
-    "message": "時停5分鐘，歐拉！"
+    title,
+    message,
   });
 });
 
@@ -12,11 +27,15 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     case "setTimer":
       browser.alarms.create("cavalry-spin", {
         delayInMinutes: 25,
-        periodInMinutes: 25,
+        periodInMinutes: 30,
+      });
+      browser.alarms.create("the-world", {
+        delayInMinutes: 30,
+        periodInMinutes: 30,
       });
       break;
     case "resetTimer":
-      browser.alarms.clear("cavalry-spin");
+      browser.alarms.clearAll();
       break;
     default:
       break;
